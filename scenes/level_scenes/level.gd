8806f4d5453faa01083df2ba59ec8acf785b1987
Edgarra1619 <edgarr.amorim@gmail.com>
2025-10-05 12:08:39 +0100

    Added the main music

--- scenes/level_scenes/level.gd
+++ scenes/level_scenes/level.gd
@@ -1,5 +1,10 @@
 extends Node3D
 
+@onready var musicplayer : AudioStreamPlayer = $AudioStreamPlayer
 
 func _ready() -> void:
 	Dialogic.start_timeline("res://resources/timeline/intro.dtl")
+	Dialogic.timeline_ended.connect(musicplayer.play)
+	
+func stop_music() -> void:
+	musicplayer.stop()
